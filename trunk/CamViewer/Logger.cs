1b520c11e78edc314c81402df81eacc7b14e9be0
romulo.penido <romulo.penido@db51f642-a2be-1cf4-444a-386b99dd281e>
2010-07-06 19:54:09 +0000

    Sistema de log

---
+++ trunk/CamViewer/Logger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace CamViewer
+{
+    static class Logger
+    {
+        static FileStream fs;
+        static StreamWriter sw;
+
+        static Logger()
+        {
+            fs = new FileStream("log.txt", FileMode.Append);
+            sw = new StreamWriter(fs);
+            WriteLine("Start session");
+        }
+
+
+        static public void WriteLine(string line)
+        {
+            sw.WriteLine("["+DateTime.Now.ToString("dd/MM/yy hh:mm:ss") + "] ("
+                + Thread.CurrentThread.ManagedThreadId + ") "
+                + line);
+            sw.Flush();
+        }
+
+        static public void WriteError(Exception e)
+        {
+            WriteLine(e.ToString());
+        }
+
+        static public void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = (Exception)e.ExceptionObject;
+            WriteError(ex);
+        }
+    }
+}
